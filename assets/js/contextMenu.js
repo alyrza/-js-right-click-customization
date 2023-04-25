fefd72bfb22b3ea0ec4e6a3db86eb52263376cc2
alyrza <alyrzanm@gmail.com>
2023-04-25 20:57:30 +0430

    fix a bug in context javascript

--- assets/js/contextMenu.js
+++ assets/js/contextMenu.js
@@ -5,6 +5,14 @@ document.addEventListener("contextmenu", (event) => {
 
   myContextMenu.style.left = event.pageX + "px";
   myContextMenu.style.top = event.pageY + "px";
+
+  if (window.innerWidth - 300 < event.pageX) {
+    myContextMenu.style.left = event.pageX - 150 + "px";
+  }
+  if (window.innerHeight - 300 < event.pageY) {
+    myContextMenu.style.top = event.pageY - 270 + "px";
+  }
+
   myContextMenu.classList.add("show");
 });
 
